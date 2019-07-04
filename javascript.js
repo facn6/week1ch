0887538251ef8ac522c9bdf536bb76e25ca53966
אולגה מולינס <OLGA@wlghs-MacBook-Air.local>
2019-07-04 14:46:14 +0300

    Links turned into HTML

--- javascript.js
+++ javascript.js
@@ -7,42 +7,28 @@ function validate() {
   var email = document.getElementById("email").value;
   var validateEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-  if ((validateEmail.test(email) == true) && (validateName.test(name) == true)){
-    document.getElementById("alert").innerHTML = "Your message has been sent successfully, we will be in touch with you ASAP.";
+  if (validateEmail.test(email) == true && validateName.test(name) == true) {
+    document.getElementById("alert").innerHTML =
+      "Your message has been sent successfully, we will be in touch with you ASAP.";
     document.getElementById("alert").style.color = "green";
     document.getElementById("display").style.display = "none";
-  } 
-
-  else if ((validateEmail.test(email) == false) && (validateName.test(name) == false)){
+  } else if (
+    validateEmail.test(email) == false &&
+    validateName.test(name) == false
+  ) {
     document.getElementById("alert").innerHTML =
       "Your email address and name are invalid!";
     document.getElementById("alert").style.color = "red";
-  } 
-  
-  else if (validateEmail.test(email) == false){
+  } else if (validateEmail.test(email) == false) {
     document.getElementById("alert").innerHTML =
       "Your email address is invalid!";
     document.getElementById("alert").style.color = "red";
-  }
-
-  else if (validateName.test(name) == false){
-    document.getElementById("alert").innerHTML =
-      "Your sure that's your name?";
+  } else if (validateName.test(name) == false) {
+    document.getElementById("alert").innerHTML = "Your sure that's your name?";
     document.getElementById("alert").style.color = "red";
-  }
-
-  else {
+  } else {
     document.getElementById("alert").innerHTML =
       "The system is down, feel free to send us an email on DontBother@NeverGonnaCheck.com";
     document.getElementById("alert").style.color = "red";
   }
 }
-
-function changeVal() {
-  window.location.href = "#Kira";
-}
-
-function changeVal2() {
-  window.location.href = "#Ghassan";
-}
-
